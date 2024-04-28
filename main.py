8825b440b3faef9bffe27c6cd656efa2484e174b
celestemarieb <34022835+celestemarieb@users.noreply.github.com>
2024-04-28 21:58:13 +1000

    add functions to create customers, sizes, retailers, size charts; entered size is now evaluated against entered body measurements

--- main.py
+++ main.py
@@ -19,42 +19,117 @@ class Size(Dimensions):
 
 class Retailer:
     def __init__(self, sizeChartID, retailerID, retailerName):
-        self.sizeChartID = 0
-        self.retailerID = 0 
-        self.retailerName = name
+        self.sizeChartID = sizeChartID
+        self.retailerID = retailerID 
+        self.retailerName = retailerName
 
 class Customer:
     def __init__(self, customerID, customerUsername, customerPassword):
         self.customerID = customerID
-        self.customerUsername = customerUserName 
+        self.customerUsername = customerUsername 
         self.customerPassword = customerPassword
 
 class SizeChart:
     def __init__(self, sizeChartID, sizeNames):
-        self.sizeChartID = 0
-        self.sizeNames = []
+        self.sizeChartID = sizeChartID
+        self.sizeNames = sizeNames
 
+# create new customer
 
-# create an instance of a body 
+def createCustomer():
+    id_input = input("ID: ")
+    username_input = input("username: ")
+    password_input = input("password: ")
+    newCustomer = Customer(id_input,username_input,password_input)
+    return newCustomer
 
-defaultBody = Body ('defaultCustomerID', 'dbust', 'dwaist', 'dhip')
+# collect dimensions
 
-print(defaultBody)
+def collectBustMeasurement():
+    bust_measurement = input("Bust: ")
+    return bust_measurement
 
-# create an instance of a size 
+def collectWaistMeasurement():
+    waist_measurement = input("Waist: ")
+    return waist_measurement
 
-defaultSize = Size('dsizename','dsizechartID','dbust','dwaist','dhip')
+def collectHipMeasurement():
+    hip_measurement = input("Hip: ")
+    return hip_measurement
+
+# collect customer measurements
+
+def collectBodyMeasurements(customerId, customer_bust,customer_waist,customer_hip): 
+    bodyInformation = Body (customerId,customer_bust,customer_waist,customer_hip)
+    return bodyInformation
+
+# collect new customer information
+
+customerInformation = createCustomer()
+customer_bust = collectBustMeasurement()
+customer_waist = collectWaistMeasurement()
+customer_hip = collectHipMeasurement()
+customer_measurements = collectBodyMeasurements(customerInformation.customerID,customer_bust,customer_waist,customer_hip)
+
+# create new retailer 
+
+def createRetailer():
+    id_input = input("Retailer ID: ")
+    name_input = input("name: ")
+    size_chart_id = input("Size Chart Id:")
+    newRetailer = Retailer (size_chart_id,id_input,name_input)
+    return newRetailer
+
+retailerInformation = createRetailer()
+
+# create size chart  
+
+def createSizeChart():
+    id_input = input("Size Chart ID: ")
+    size_names_input = []
+    newSizeChart = SizeChart (id_input,size_names_input)
+    return newSizeChart 
+
+sizeChartInformation = createSizeChart()
+
+def getSizeChartID(sizeChartInformation):
+    sizeChartID = sizeChartInformation.sizeChartID
+    return sizeChartID
+
+newSizeChartID = getSizeChartID(sizeChartInformation)
+    
+# create size 
+
+def createSize():
+    name_input = input("Size Name: ")
+    sizeChartInformation.sizeNames.append(name_input)
+    size_bust = collectBustMeasurement()
+    size_waist = collectWaistMeasurement()
+    size_hip = collectHipMeasurement()
+    newSize = Size(name_input,newSizeChartID,size_bust,size_waist,size_hip)
+    return newSize
+
+size_measurements = createSize()
 
 # evaluate 'fit' 
 
-fit = True 
+def evaluateFit(customer_measurements,size_measurements):
+    fit = True  
+    if customer_measurements.bust == size_measurements.bust and customer_measurements.waist == size_measurements.waist and customer_measurements.hip == size_measurements.hip:
+        fit = True 
+    else: fit = False
+    print(fit)
 
-if defaultBody.bust == defaultSize.bust and defaultBody.waist == defaultSize.waist and defaultBody.hip == defaultSize.hip:
-    fit = True 
-else: fit = False
+fit_determination = evaluateFit(customer_measurements,size_measurements)
 
-# print result 
+# create customer database - dictionary 
 
-print(fit)
+# store in customer database 
 
+# create retailer database 
 
+# create sizing database 
+
+# store sizechart 
+
+# upload csvs to databases 
